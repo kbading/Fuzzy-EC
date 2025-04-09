73db88cca65b12e5ce87567389ec118e99aa21de
mariusbarth <marius.barth@uni-koeln.de>
2025-04-09 14:40:41 +0200

    wsw2 study: added wsw8b model with ec effects as predictor

---
+++ studies/wsw2-main/R/trait-mpt-wsw-8b-with-ec.R
@@ -0,0 +1,36 @@
+
+library(TreeBUGS)
+
+project_root <- rprojroot::find_rstudio_root_file()
+study_folder <- file.path(project_root, "studies", "wsw2-main")
+
+data_list <- readRDS(file.path(study_folder, "data", "data.rds"))
+mpt_data_hierarchical <- merge(data_list$mpt_data_hierarchical, data_list$rating_wide, sort = FALSE)
+stopifnot(all(mpt_data_hierarchical$sid == data_list$mpt_data_hierarchical$sid))
+
+mpt_data_hierarchical <- within(
+  mpt_data_hierarchical
+  , {
+    ec_rating_first <- ec_effect * (task_order == "Rating first")
+    ec_memory_first <- ec_effect * (task_order == "Memory first")
+  }
+)
+rownames(mpt_data_hierarchical) <- mpt_data_hierarchical$sid
+
+model <- traitMPT(
+  , eqnfile = file.path(project_root, "model-equations", "wsw-8b.eqn")
+  , data = mpt_data_hierarchical
+  , n.adapt  = 2e4L
+  , n.burnin = 1e5L
+  , n.iter   = 2e5L
+  , n.thin   = 2e1L
+  , n.chains = 4e0L
+  , ppp      = 5e3L
+  , restrictions = list("G = 0.25")
+  , covData = subset(mpt_data_hierarchical, select = "task_order")
+  , predStructure = list("a b D d_positive d_negative C_positive C_negative ; task_order ec_rating_first ec_memory_first")
+  , predType = c("f", "c", "c")
+)
+
+dir.create(file.path(study_folder, "model-objects"), showWarnings = FALSE)
+saveRDS(model, file = file.path(study_folder, "model-objects", "trait-mpt-wsw-8b-with-ec.rds"))
