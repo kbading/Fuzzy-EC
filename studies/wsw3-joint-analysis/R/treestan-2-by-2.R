4b2736cd5ada35e3f58b886c63804ba4e4af406f
mariusbarth <marius.barth@uni-koeln.de>
2025-10-17 09:16:40 +0200

    wsw3: added interaction model

---
+++ studies/wsw3-joint-analysis/R/treestan-2-by-2.R
@@ -0,0 +1,27 @@
+
+library(TreeStan)
+treestan_options(refresh = 0)
+
+project_root <- rprojroot::find_rstudio_root_file()
+study_folder <- file.path(project_root, "studies", "wsw3-joint-analysis")
+
+mpt_data_hierarchical <- readRDS(file.path(study_folder, "data", "data.rds"))$mpt_data_hierarchical
+contrasts(mpt_data_hierarchical$task_focus) <- "contr.sum"
+contrasts(mpt_data_hierarchical$study     ) <- "contr.sum"
+
+model <- TreeStan::fit_mpt(
+  eqn_file = file.path(study_folder, "WSW_exp3_hierarchical.eqn")
+  , restrictions = list(G = 1/8)
+  , data = mpt_data_hierarchical
+  , formula = ~ task_focus * study
+  , lm_y = mpt_data_hierarchical$ec_effect
+  , warmup  = 2e3L
+  , iter    = 4e3L
+  , chains  = 8e0L
+  , cores   = parallel::detectCores()
+  , refresh = if(interactive()) 100 else 0
+)
+
+dir.create(file.path(study_folder, "model-objects"), showWarnings = FALSE)
+saveRDS(model, file = file.path(study_folder, "model-objects", "treestan-2-by-2.rds"))
+summary(model, pars = "beta")
